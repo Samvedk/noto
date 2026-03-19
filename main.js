100b656b4e714d22205315a004b1b5444f861f6a
Samved | Noto <samved@MacBookAir.lan>
2026-03-19 15:14:03 +0530

    clean push

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 750,
+    autoHideMenuBar: true
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
